36037decb78e25f7eda5a7f8f2dd4f882e90a3a5
Denis Polyakov <p-ov.da@ya.ru>
2022-07-30 15:17:44 +0300

    Fix staticfiles

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -120,9 +120,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles'),
-]
+STATICFILES_DIRS = []
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
